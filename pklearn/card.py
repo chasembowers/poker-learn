7391bde2839c770ebb1ddc0a5a826c3a6186434f
Chase <chasembowers@gmail.com>
2015-10-06 22:27:58 -0500

    Changed card members to public

--- pklearn/card.py
+++ pklearn/card.py
@@ -2,8 +2,8 @@ from deuces import deuces
 
 class Card:
     
-    _nEnum = {'T':10, 'J':11, 'Q':12, 'K':13, 'A':14}
-    _suits = ['c', 'd', 's', 'h']
+    nEnum = {'T':10, 'J':11, 'Q':12, 'K':13, 'A':14}
+    suits = ['c', 'd', 's', 'h']
 
     def __init__(self, numLet, suit):
 
@@ -12,14 +12,14 @@ class Card:
         if type(numLet) == int:
             if numLet < 2 or numLet > 14: raise Exception('Card number must be between 2 and 14 (inclusive).')
             self._numLet = numLet
-            for k in self._nEnum:
-                if self._nEnum[k] == numLet: self._numLet = k
+            for k in self.nEnum:
+                if self.nEnum[k] == numLet: self._numLet = k
         elif type(numLet) == str:
-            if numLet.upper() not in self._nEnum: raise Exception("Card letter must be \'T\', \'J\', \'Q\', \'K\', or \'A\'.")
+            if numLet.upper() not in self.nEnum: raise Exception("Card letter must be \'T\', \'J\', \'Q\', \'K\', or \'A\'.")
             self._numLet = numLet.upper()
         else: raise Exception('Card number/letter must be number or string.')
 
-        if suit.lower() not in self._suits: raise Exception("Invalid suit. Valid suits are \'c\', \'d\', \'s\', and \'h\'.")
+        if suit.lower() not in self.suits: raise Exception("Invalid suit. Valid suits are \'c\', \'d\', \'s\', and \'h\'.")
         self._suit = suit.lower()
 
     def __str__(self): return str(self._numLet) + self._suit
@@ -30,7 +30,7 @@ class Card:
         
         """ This method returns number of card. It converts letter to number if necesary. """
         
-        if self._numLet in self._nEnum: return self._nEnum[self._numLet]
+        if self._numLet in self.nEnum: return self.nEnum[self._numLet]
         return self._numLet
 
     def getSuit(self): return self._suit
